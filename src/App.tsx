ddf4dc5b46b2fd977e6c9082cbaa2d818940909d
Damilola Majek <kumbi_majek@yahoo.com>
2021-11-04 00:29:15 +0100

    Add chat background, chatbox UI and input wrapper section

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div>
+			<div className="container">
+				<div className="chat-wrapper">
+					<div className="recipent chat-box">
+						<span className="name">Dami</span>
+						<p className="message">Sounds good to me</p>
+						<span className="date">10 Mar 2018 10:22</span>
+					</div>
+					<div className="sender chat-box">
+						<p className="message">
+							Thanks alot Dami for these updates
+						</p>
+						<span className="date">10 Mar 2018 10:26</span>
+					</div>
+				</div>
+			</div>
+			<div className="input-group">
+				<div className="container">
+					<form>
+						<input type="text" placeholder="Message" />
+						<input type="submit" value="Send" />
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default App;
