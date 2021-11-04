88c363848538854550d981a8b9f6aa1b260795e7
Damilola Majek <kumbi_majek@yahoo.com>
2021-11-04 01:19:10 +0100

    Add logic to send message and fetch messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,53 @@
+import api from "./components/api";
+import { useEffect, useState, useRef, FormEvent } from "react";
+
 const App = () => {
+	const [inputMessage, setInputMessage] = useState<string>("");
+	const [messages, setMessages] = useState<any>([]);
+
+	const chatWrapper = useRef<HTMLDivElement>(null);
+
+	const sender = "Flora";
+
+	useEffect(() => {
+		getMessages();
+		setInterval(getMessages, 5000);
+	}, []);
+
+	const getMessages = () => {
+		api.get("")
+			.then((response) => {
+				if (response.data.length > messages.length) {
+					setMessages(response.data);
+				}
+				console.log(response);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	};
+
+	const sendMessage = (e: FormEvent) => {
+		e.preventDefault();
+		if (inputMessage.length > 0) {
+			api.post("", {
+				message: inputMessage,
+				author: sender,
+			})
+				.then(() => {
+					getMessages();
+				})
+				.catch((error) => {
+					console.error(error);
+				});
+
+			setInputMessage("");
+		}
+	};
+
 	return (
 		<div>
-			<div className="container">
+			<div className="container" ref={chatWrapper}>
 				<div className="chat-wrapper">
 					<div className="recipent chat-box">
 						<span className="name">Dami</span>
@@ -18,8 +64,13 @@ const App = () => {
 			</div>
 			<div className="input-group">
 				<div className="container">
-					<form>
-						<input type="text" placeholder="Message" />
+					<form onSubmit={sendMessage}>
+						<input
+							type="text"
+							placeholder="Message"
+							value={inputMessage}
+							onChange={(e) => setInputMessage(e.target.value)}
+						/>
 						<input type="submit" value="Send" />
 					</form>
 				</div>
